da41fe6207816fc1a799a52d5e93d20153550436
sweerasingha <sachiprojects@gmail.com>
2024-01-14 10:22:47 +0530

    Initial commit

---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,2 @@
+
+String loginUrlImage = 'https://wallpapercave.com/wp/wp4258271.jpg';
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'LoginPage/login_screen.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text(
+                  "Volunteer Connect App is being initialized",
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Signatra'
+                  ),
+                ),
+              ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text(
+                  "An error has occurred: ${snapshot.error}",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Signatra'
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+        // Replace 'const Scaffold()' with your main screen widget.
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Volunteer Connect',
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.red,
+            primarySwatch: Colors.blue,
+          ),
+          home: Login(),
+        );
+      },
+    );
+  }
+}
