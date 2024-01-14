2c51c5b3c4a92c58458e586925ff5f8c379744eb
sweerasingha <sachiprojects@gmail.com>
2024-01-14 18:51:15 +0530

    Bottom Navigation Bar Successfully Implemented

---
+++ lib/Jobs/upload_job.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../Widgets/bottom_nav_bar.dart';
+
+class UploadJobNow extends StatefulWidget {
+
+  @override
+  State<UploadJobNow> createState() => _UploadJobNowState();
+}
+
+class _UploadJobNowState extends State<UploadJobNow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepOrange.shade300, Colors.blueAccent],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          stops: const [0.2,0.9],
+        ),
+      ),
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBarForApp(indexNum: 2,),
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text(
+            "Upload Job Now",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Signatra'
+            ),
+          ),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.deepOrange.shade300, Colors.blueAccent],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                stops: const [0.2,0.9],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Widgets/bottom_nav_bar.dart
+++ lib/Widgets/bottom_nav_bar.dart
@@ -1,8 +1,10 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:vc_v1/Jobs/upload_job.dart';
 import 'package:vc_v1/Search/search_companies.dart';
 
 import '../Jobs/jobs_screen.dart';
+import '../Search/pofile_company.dart';
 
 class BottomNavigationBarForApp extends StatelessWidget {
 
@@ -21,7 +23,8 @@ class BottomNavigationBarForApp extends StatelessWidget {
       items: [
         Icon(Icons.list, size: 19, color: Colors.black),
         Icon(Icons.search, size: 19, color: Colors.black),
-
+        Icon(Icons.add, size: 19, color: Colors.black),
+        Icon(Icons.person_pin, size: 19, color: Colors.black),
       ],
       animationDuration: Duration(
         milliseconds: 300,
@@ -37,6 +40,15 @@ class BottomNavigationBarForApp extends StatelessWidget {
             {
               Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => AllWorkersScreen()));
             }
+          else if (index == 2)
+            {
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => UploadJobNow()));
+            }
+          else if (index == 3)
+            {
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => ProfileScreen()));
+            }
+
         }
     );
   }
