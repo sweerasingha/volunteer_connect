25ddc652ff08e9c6d49e5b58983d709018f6c746
sweerasingha <sachiprojects@gmail.com>
2024-01-18 14:23:40 +0530

    Search jobs successfully implemented

--- lib/Search/search_job.dart
+++ lib/Search/search_job.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:vc_v1/Jobs/jobs_screen.dart';
+import 'package:vc_v1/Widgets/job_widget.dart';
 
 class SearchScreen extends StatefulWidget {
 
@@ -7,6 +10,56 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+
+  final TextEditingController _searchQueryController = TextEditingController();
+  String seachQuery = "Search query";
+
+  Widget _buildSearchField()
+  {
+    return TextField(
+      controller: _searchQueryController,
+      autofocus: true,
+      decoration: const InputDecoration(
+        hintText: 'Search for jobs...',
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: Colors.white),
+      ),
+      style: const TextStyle(color: Colors.white, fontSize: 16.0),
+      onChanged: (query) => updateSearchQuery(query),
+
+    );
+  }
+
+  List<Widget> _buildActions()
+  {
+    return <Widget>[
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: (){
+          _clearSearchQuery();
+        },
+      ),
+    ];
+  }
+
+  void _clearSearchQuery()
+  {
+    setState(() {
+      _searchQueryController.clear();
+      updateSearchQuery("");
+    });
+  }
+
+  void updateSearchQuery(String newQuery)
+  {
+    setState(() {
+      seachQuery = newQuery;
+      print(seachQuery);
+    });
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,15 +74,6 @@ class _SearchScreenState extends State<SearchScreen> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
-          title: const Text(
-            'Search',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          centerTitle: true,
           flexibleSpace: Container(
             decoration: BoxDecoration(
               gradient: LinearGradient(
@@ -40,6 +84,68 @@ class _SearchScreenState extends State<SearchScreen> {
               ),
             ),
           ),
+          leading: IconButton(
+            onPressed: (){
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> JobScreen()));
+            },
+            icon: const Icon(Icons.arrow_back),
+          ),
+          title: _buildSearchField(),
+          actions: _buildActions(),
+        ),
+        body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: FirebaseFirestore.instance
+              .collection('jobs')
+              .where('jobTitle', isGreaterThanOrEqualTo: seachQuery)
+              .where('recruitment', isEqualTo: true)
+              .snapshots(),
+          builder: (context, AsyncSnapshot snapshot)
+          {
+            if(snapshot.connectionState == ConnectionState.waiting)
+              {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            else if(snapshot.connectionState == ConnectionState.active)
+              {
+                if(snapshot.data?.docs.isNotEmpty == true)
+                  {
+                    return ListView.builder(
+                      itemCount: snapshot.data?.docs.length,
+                      itemBuilder: (BuildContext context, int index){
+                        return JobWidget(
+                          jobTitle: snapshot.data?.docs[index]['jobTitle'],
+                          jobDescription: snapshot.data?.docs[index]['jobDescription'],
+                          jobId: snapshot.data?.docs[index]['jobId'],
+                          uploadedBy: snapshot.data?.docs[index]['uploadedBy'],
+                          userImage: snapshot.data?.docs[index]['userImage'],
+                          name: snapshot.data?.docs[index]['name'],
+                          recruitment: snapshot.data?.docs[index]['recruitment'],
+                          email: snapshot.data?.docs[index]['email'],
+                          location: snapshot.data?.docs[index]['location'],
+                        );
+                      },
+                    );
+                  }
+                else
+                  {
+                    return const Center(
+                      child: Text('No jobs found'),
+                    );
+                  }
+              }
+            return Center(
+              child: Text(
+                'Something went wrong',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
